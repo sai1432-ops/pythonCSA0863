9f4c78cdfdaaf155c9deb4c88aa0ac9f1768ad32
sai1432-ops <112856759+sai1432-ops@users.noreply.github.com>
2023-02-09 23:10:26 +0530

    Add files via upload

---
+++ day 2.2.py
@@ -0,0 +1,6 @@
+def checkyear(year):
+    if(year%4==0):
+        print("leap year")
+    else:
+        print("not leap year")
+checkyear(2001)
---
+++ day 2.4.py
@@ -0,0 +1,6 @@
+a=[1,2,3,4,9,6,0]
+b=[5,3,7,4,9,6,7]
+a.extend(b)
+print(a)
+a.sort()
+print(a)
